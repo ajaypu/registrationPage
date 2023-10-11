29a7437e33a65e5de6723996d3606afbf0ee1c55
Ajay Prakash <146950951+ajaypu@users.noreply.github.com>
2023-10-11 12:44:17 +0530

    Consoling  input values

--- script.js
+++ script.js
@@ -4,19 +4,11 @@ const inputEmail = document.querySelector("#email");
 const inputPhone = document.querySelector("#phone");
 const inputDate = document.querySelector("#date");
 const inputTime = document.querySelector("#time");
-
-const obj = {
-  name,
-  email,
-  phone,
-};
 //
 form.addEventListener("submit", onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  obj.name = inputName.value;
-  obj.email = inputEmail.value;
-  obj.phone = inputPhone.value;
-  localStorage.setItem(obj.email, JSON.stringify(obj));
+  console.log(inputName.value,inputEmail.value,inputPhone.value,inputDate.value,inputTime.value)
+  
 }
