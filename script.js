972658d25708d68c735a91b5c9acaf5d4b72fbc2
Ajay Prakash <21376002@pondiuni.ac.in>
2023-10-11 11:50:59 +0530

    updating local Storage

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const inputPhone = document.querySelector("#phone");
 const inputDate = document.querySelector("#date");
 const inputTime = document.querySelector("#time");
 
+const obj = {
+  name,
+  email,
+  phone,
+};
+//
 form.addEventListener("submit", onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  console.log(inputName.value);
-  console.log(inputEmail.value);
-  console.log(inputPhone.value);
-  console.log(inputDate.value);
-  console.log(inputTime.value);
+  obj.name = inputName.value;
+  obj.email = inputEmail.value;
+  obj.phone = inputPhone.value;
+  localStorage.setItem(obj.email, JSON.stringify(obj));
 }
